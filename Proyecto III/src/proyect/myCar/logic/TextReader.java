0ac87cda9d7bc1f43682d950b25aee549343b44e
dnnxl15 <dnnxl15@hotmail.com>
2017-06-08 09:36:21 -0600

    Add methods to the TextReader Class, like read, and it apply singleton to the class TextReader.

---
+++ Proyecto III/src/proyect/myCar/logic/TextReader.java
@@ -0,0 +1,49 @@
+package proyect.myCar.logic;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TextReader 
+{
+	private static TextReader Instance; //instance
+
+	private TextReader()
+	{
+	}
+	
+	public synchronized static TextReader getInstance() //synchronized the TextReader
+	{
+		if (Instance==null)
+		{
+			Instance = new TextReader();
+		}
+		return Instance;
+	}
+	
+	public String readFile(String pFile)  //read the file
+	{
+		String Text = "";
+	    try
+	    { 
+	    	File TextFile = new File(pFile); //TextFile of the File type
+	    	FileReader ReaderText = new FileReader(TextFile); //ReaderText of the FileReader type
+	    	BufferedReader ReaderBuffer = new BufferedReader(ReaderText); //ReaderBuffer of the BufferedReader type
+	   		String Line;
+	   		String Chain = "";
+	   		while ((Line = ReaderBuffer.readLine()) != null) //read each line
+	   		{
+	   			Chain = Chain + Line;
+    		}
+	    	Text = Chain; //Text equal to CHain
+	    	ReaderBuffer.close();
+	   	}
+	   	catch(Exception e)
+	   	{
+    	}
+	    return Text;
+	}
+}
