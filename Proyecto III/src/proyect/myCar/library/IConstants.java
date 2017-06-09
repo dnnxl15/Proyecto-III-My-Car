b123259455dc539b07bf5cc39303a15d535298b7
dnnxl15 <dnnxl15@hotmail.com>
2017-06-09 11:22:55 -0600

    Add comments

--- Proyecto III/src/proyect/myCar/library/IConstants.java
+++ Proyecto III/src/proyect/myCar/library/IConstants.java
@@ -2,11 +2,6 @@ package proyect.myCar.library;
 
 public interface IConstants 
 {
-<<<<<<< HEAD
-public final int x = 0;
-
+	public final int X = 0;//Eje x
 	public final int MOUNT_OF_THREADS = 5; // Mount of thread 
-=======
-	public final int MOUNT_OF_THREADS = 5;
->>>>>>> refs/remotes/origin/master
 }
--- Proyecto III/src/proyect/myCar/library/IObservable.java
+++ Proyecto III/src/proyect/myCar/library/IObservable.java
@@ -5,9 +5,4 @@ public interface IObservable
 	void addObserver(IObserver pObserver);
 	void removeObserver(IObserver pObserver);
 	void notifyObserver();
-<<<<<<< HEAD
-
 }
-=======
-}
->>>>>>> refs/remotes/origin/master
