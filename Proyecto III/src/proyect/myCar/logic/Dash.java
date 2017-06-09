4af7ef67a3dd16e3df56689f33e2aefac6cd9054
dnnxl15 <dnnxl15@hotmail.com>
2017-06-08 22:55:39 -0600

    Dash implement IObservable

--- Proyecto III/src/proyect/myCar/logic/Dash.java
+++ Proyecto III/src/proyect/myCar/logic/Dash.java
@@ -1,5 +1,22 @@
 package proyect.myCar.logic;
 
-public class Dash {
+import proyect.myCar.library.IObservable;
+import proyect.myCar.library.IObserver;
 
+public class Dash implements IObservable
+{
+
+	@Override
+	public void addObserver(IObserver observer) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeObserver(IObserver observer) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
