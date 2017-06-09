4e2a7fe1998dd672bb02225e058fbdf809d8874e
GuilinNEX <esteb@RobertoCoto>
2017-06-09 11:08:09 -0600

    Add commments

--- Proyecto III/src/proyect/myCar/library/IConstants.java
+++ Proyecto III/src/proyect/myCar/library/IConstants.java
@@ -3,4 +3,6 @@ package proyect.myCar.library;
 public interface IConstants 
 {
 public final int x = 0;
+
+	public final int MOUNT_OF_THREADS = 5;
 }
--- Proyecto III/src/proyect/myCar/library/IObservable.java
+++ Proyecto III/src/proyect/myCar/library/IObservable.java
@@ -2,6 +2,8 @@ package proyect.myCar.library;
 
 public interface IObservable 
 {
- 
+	void addObserver(IObserver pObserver);
+	void removeObserver(IObserver pObserver);
+	void notifyObserver();
 
 }
